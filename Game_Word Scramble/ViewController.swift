bd9bddf1aa81031671ecd2f486366bf39d711883
Kim - leo <ochell2@naver.com>
2021-11-30 16:04:57 +0900

    Initial Commit

---
+++ Game_Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game_Word Scramble
+//
+//  Created by 김승현 on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
